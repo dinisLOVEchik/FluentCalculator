44b1c334d8d46e265cb2a2d1caf64a2b1d299cf9
dinisLOVEchik <mr.saburlaev@mail.ru>
2024-06-17 10:37:59 +0300

    Добавление проекта в репозиторий

---
+++ FluentCalculator.cs
@@ -0,0 +1,67 @@
+﻿namespace Calculator
+{
+    public class FluentCalculator
+    {
+        private readonly List<int> values = new List<int>();
+        private readonly List<string> operations = new List<string>();
+        private bool waitingForOperation = true;
+
+        private static Dictionary<string, int> valueMap = new Dictionary<string, int>
+        {
+            {"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4},
+            {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9},
+            {"ten", 10}
+        };
+
+        public FluentCalculator() { }
+
+        public FluentCalculator Value(string value)
+        {
+            if (!waitingForOperation)
+            {
+                throw new Exception("Invalid method call sequence");
+            }
+            waitingForOperation = false;
+            values.Add(valueMap[value]);
+            return this;
+        }
+
+        public FluentCalculator Operation(string operation)
+        {
+            if (waitingForOperation)
+            {
+                throw new Exception("Invalid method call sequence");
+            }
+            waitingForOperation = true;
+            operations.Add(operation);
+            return this;
+        }
+
+        public decimal Result()
+        {
+            decimal result = values[0];
+            for (int i = 0; i < operations.Count; i++)
+            {
+                switch (operations[i])
+                {
+                    case "plus":
+                        result += values[i + 1];
+                        break;
+                    case "minus":
+                        result -= values[i + 1];
+                        break;
+                    case "times":
+                        result *= values[i + 1];
+                        break;
+                    case "dividedBy":
+                        result /= values[i + 1];
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation");
+                        break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Calculator;
+
+FluentCalculator calculator = new FluentCalculator();
+var result = calculator.Value("one").Operation("plus")
+    .Value("two").Operation("minus").Value("four").Operation("times").Value("nine")
+    .Operation("dividedBy").Value("three").Result();
+Console.WriteLine(result);
+
